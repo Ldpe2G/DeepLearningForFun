b06ebd5e68c5738b765254b0fabfd9acd7520e41
梁德澎 <liangdepeng@gmail.com>
2017-05-29 19:38:26 +0800

    Update train_cycle_gan.sh

--- Mxnet-Scala/CycleGAN/scripts/train_cycle_gan.sh
+++ Mxnet-Scala/CycleGAN/scripts/train_cycle_gan.sh
@@ -1,7 +1,7 @@
 ROOT=$(cd "$(dirname $0)/.."; pwd)
 
 # put your mxnet jar file in the $ROOT/lib folder
-MXNET_JAR_FILE=$ROOT/lib/mxnet-full_2.11-linux-x86_64-gpu-0.9.5-SNAPSHOT.jar
+MXNET_JAR_FILE=$ROOT/lib/mxnet-full_2.11-linux-x86_64-gpu-0.10.0-SNAPSHOT.jar
 
 CLASS_PATH=$MXNET_JAR_FILE:$ROOT/target/scala-2.11/classes/:\
 $HOME/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.11.8.jar:\
